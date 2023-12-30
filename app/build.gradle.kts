f14e4286052322f8bf163fe51124899ee8ef4732
PKrystian <krystian.pinczak2001@gmail.com>
2023-12-30 13:24:54 +0100

    styling fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,16 +3,21 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
+val minSdkVersion: Int = 26
+val targetSdkVersion: Int = 34
+val appVersionCode: Int = 1
+val appVersionName: String = "1.0"
+
 android {
     namespace = "com.example.fivenightsescape"
     compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.fivenightsescape"
-        minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = minSdkVersion
+        targetSdk = targetSdkVersion
+        versionCode = appVersionCode
+        versionName = appVersionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -36,7 +41,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
