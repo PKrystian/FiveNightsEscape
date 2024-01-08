bb6a7ea7a2d61d61b7caa1b77b1cb6304628082e
Krystian Pińczak <krystian.pinczak2001@gmail.com>
2024-01-08 19:17:28 +0100

    7 use of intentions create xml layouts (#20)
    * [XML-layouts] Added 2 layouts for menu and game + implemented it
    
    * lorem ipsum clear code fix

--- app/src/main/java/com/example/fivenightsescape/MapsActivity.kt
+++ app/src/main/java/com/example/fivenightsescape/MapsActivity.kt
@@ -41,6 +41,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
+//        For now this will give an error ( magic number ), but we will remove this element later.
         var sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
