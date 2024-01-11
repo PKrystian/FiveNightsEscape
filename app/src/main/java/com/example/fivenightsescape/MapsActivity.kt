20f4f9f8f36bb90c839fef9d765878d6434706b3
AntPog <anti0282@gmail.com>
2024-01-11 21:39:30 +0100

    added timer, levels, progressbar

--- app/src/main/java/com/example/fivenightsescape/MapsActivity.kt
+++ app/src/main/java/com/example/fivenightsescape/MapsActivity.kt
@@ -5,7 +5,11 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Chronometer
 import android.widget.Toast
+import android.os.Handler
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -28,13 +32,33 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var mMap: GoogleMap
 
+    //timer and lv things
+    private var levelChanger = 4 //levelChanger * delay = every how many milliseconds next lv, it changes every two levels
+    private val delay: Long = 500 // how often does the progressbar update
+    private var currentLevel = 1 //lv the player is on
+    private var subLevel = 0 // for the progressbar and to change level length
+    private val handlerTimer = Handler()
+    private lateinit var levelText: TextView
+    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
+        //timer
+        val timerONE :Chronometer = findViewById(R.id.timerONE)
+        timerONE.start()
+        handlerTimer.postDelayed(newLevel, delay)
+        //level indicator
+        levelText = findViewById(R.id.levelIndicator)
+        //progress bar
+        progressBar = findViewById(R.id.progressBar)
+
+
         val stopButton: Button = findViewById(R.id.stopButton)
 
         stopButton.setOnClickListener {
+            timerONE.stop() //stops timer
+            handlerTimer.removeCallbacks(newLevel) //stops level counting
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
@@ -43,6 +67,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
+    //function to change levels
+    private val newLevel = object : Runnable {
+        override fun run() {
+            runOnUiThread {
+                if (subLevel<levelChanger){
+                    subLevel+=1
+                    progressBar.progress = (subLevel.toDouble() / levelChanger * 100).toInt()
+                }
+                else{
+                    if (currentLevel%2==0){
+                        levelChanger*=2 // every two levels it takes longer to get another lv
+                    }
+                    progressBar.progress = 0
+                    subLevel = 0
+                    currentLevel +=1
+                    levelText.text = "Level: " + currentLevel.toString()
+                }
+            }
+            handlerTimer.postDelayed(this, delay)
+        }
+    }
     override fun onMapReady(
         googleMap: GoogleMap
     ) {
