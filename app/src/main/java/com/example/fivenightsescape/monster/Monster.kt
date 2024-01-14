f19acba4a5e8505494836d081062b7ba0c9f677c
Mikolaj Bigaj <mikolaj.bigaj.00@gmail.com>
2024-01-14 16:43:37 +0100

    Added changes to fix tests not passing, converted MagicNumbers to consts used in Monster class

--- app/src/main/java/com/example/fivenightsescape/monster/Monster.kt
+++ app/src/main/java/com/example/fivenightsescape/monster/Monster.kt
@@ -13,15 +13,33 @@ const val TYPE_STANDING = "standing"
 const val TYPE_MOVING = "moving"
 const val TYPE_WANDERING = "wandering"
 
+const val MONSTER_DAMAGE: Int = 1
+
+const val MONSTER_ZERO_SPEED: Double = 0.0
+const val MONSTER_CHASING_SPEED: Double = 0.0001
+const val MONSTER_WANDERING_SPEED: Double = 0.00005
+
+const val MONSTER_ZERO_RANGE: Double = 0.0
+const val MONSTER_TYPE_STANDING_RANGE: Double = 10.0
+const val MONSTER_TYPE_MOVING_RANGE: Double = 10.0
+const val MONSTER_TYPE_WANDERING_RANGE: Double = 10.0
+
+const val MONSTER_DEFAULT_COLOR = Color.BLACK
+var MONSTER_TYPE_STANDING_COLOR = Color.rgb(130, 126, 12)
+var MONSTER_TYPE_MOVING_COLOR = Color.rgb(255, 0, 0)
+var MONSTER_TYPE_WANDERING_COLOR = Color.rgb(204, 111, 24)
+
+const val MONSTER_WANDER_OFFSET: Double = 0.0005
+
 abstract class Monster(
-    val damage: Int = 1,
+    val damage: Int = MONSTER_DAMAGE,
     var player: Player,
     var position: LatLng,
     var mMap: GoogleMap)
 {
-    var speed: Double = 0.0
-    var range: Double = 10.0
-    var color: Int = Color.BLACK
+    var speed: Double = MONSTER_ZERO_SPEED
+    var range: Double = MONSTER_ZERO_RANGE
+    var color: Int = MONSTER_DEFAULT_COLOR
     var monsterType: String = ""
     var location: Location = Location("")
 
@@ -74,8 +92,8 @@ class MonsterStanding(
 ): Monster(damage, player, position, mMap) {
     init {
         this.monsterType = TYPE_STANDING
-        this.range = 10.0
-        this.color = Color.rgb(130, 126, 12)
+        this.range = MONSTER_TYPE_STANDING_RANGE
+        this.color = MONSTER_TYPE_STANDING_COLOR
 
         this.initCircle()
     }
@@ -89,9 +107,9 @@ class MonsterMoving(
 ): Monster(damage, player, position, mMap) {
     init {
         this.monsterType = TYPE_MOVING
-        this.speed = 0.0001
-        this.range = 20.0
-        this.color = Color.RED
+        this.speed = MONSTER_CHASING_SPEED
+        this.range = MONSTER_TYPE_MOVING_RANGE
+        this.color = MONSTER_TYPE_MOVING_COLOR
 
         this.initCircle()
     }
@@ -105,9 +123,9 @@ class MonsterWandering(
 ): Monster(damage, player, position, mMap) {
     init {
         this.monsterType = TYPE_WANDERING
-        this.speed = 0.0001
-        this.range = 15.0
-        this.color = Color.rgb(204, 111, 24)
+        this.speed = MONSTER_WANDERING_SPEED
+        this.range = MONSTER_TYPE_WANDERING_RANGE
+        this.color = MONSTER_TYPE_WANDERING_COLOR
 
         this.initCircle()
         this.initializeWandering()
@@ -115,11 +133,11 @@ class MonsterWandering(
 
     fun initializeWandering()
     {
-        val fromLatitude = this.position.latitude - Random.nextDouble(until = 0.0005)
-        val fromLongitude = this.position.longitude - Random.nextDouble(until = 0.0005)
+        val fromLatitude = this.position.latitude - Random.nextDouble(until = MONSTER_WANDER_OFFSET)
+        val fromLongitude = this.position.longitude - Random.nextDouble(until = MONSTER_WANDER_OFFSET)
 
-        val toLatitude = this.position.latitude + Random.nextDouble(until = 0.0005)
-        val toLongitude = this.position.longitude + Random.nextDouble(until = 0.0005)
+        val toLatitude = this.position.latitude + Random.nextDouble(until = MONSTER_WANDER_OFFSET)
+        val toLongitude = this.position.longitude + Random.nextDouble(until = MONSTER_WANDER_OFFSET)
 
         this.wanderFrom.latitude = fromLatitude
         this.wanderFrom.longitude = fromLongitude
